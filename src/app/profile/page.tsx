f6ca8fbd6727acfae7c12b98a16b55191c90731a
eemiljka <eemiljka@metropolia.fi>
2025-04-10 14:48:05 +0300

    changePassword works but not hashing, fix later

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,12 +2,13 @@
 
 import Sidebar from "@/components/Sidebar";
 import Divider from "@/components/Divider";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Modal from "react-modal";
 import { PencilIcon } from "@heroicons/react/20/solid";
 import { useGetUserInfo } from "@/hoooks/apiHooks";
 import { User } from "@/types/DBTypes";
 import UpdateUsernameForm from "../username/page";
+import UpdatePasswordForm from "../password/page";
 
 const Profile: React.FC = () => {
   const { profileInfo, profileIsLoading, profileError, refetchUserInfo } =
@@ -16,12 +17,23 @@ const Profile: React.FC = () => {
   const [changeUsernameModalIsOpen, setChangeUsernameModalIsOpen] =
     React.useState(false);
 
+  const [changePasswordModalIsOpen, setChangePasswordModalIsOpen] =
+    React.useState(false);
+
   function openChangeUsernameModal() {
     setChangeUsernameModalIsOpen(true);
   }
 
   function closeChangeUsernameModal() {
     setChangeUsernameModalIsOpen(false);
+  }
+
+  function openChangePasswordModal() {
+    setChangePasswordModalIsOpen(true);
+  }
+
+  function closeChangePasswordModal() {
+    setChangePasswordModalIsOpen(false);
   }
 
   useEffect(() => {
@@ -117,9 +129,23 @@ const Profile: React.FC = () => {
             <p className="mt-10">Password</p>
             <div className="flex justify-between">
               <h3 className="text-xl font-semibold">******************</h3>
-              <button className="hover:bg-zinc-100 rounded-md p-1">
+              <button
+                onClick={openChangePasswordModal}
+                className="hover:bg-zinc-100 rounded-md p-1"
+              >
                 <PencilIcon className="w-5 h-5 text-zinc-600" />
               </button>
+              <Modal
+                className={"bg-white rounded-lg shadow-md p-8"}
+                style={customStyles}
+                isOpen={changePasswordModalIsOpen}
+                onRequestClose={closeChangePasswordModal}
+                contentLabel="Change Password"
+              >
+                <UpdatePasswordForm
+                  closeChangePasswordModal={closeChangePasswordModal}
+                />
+              </Modal>
             </div>
           </div>
         </div>
